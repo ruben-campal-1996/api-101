9d3043fdccad87c965816017de33c920ca1887f6
Rubén Campal <ruben.campal.lopez1996@gmail.com>
2026-05-05 00:25:04 +0200

    add 2nd exercise

---
+++ backend/exercise/ejercicio2.js
@@ -0,0 +1,33 @@
+/* async function listPokemons() {  
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon");  
+  const data = await response.json();  
+
+  console.log("Lista de Pokémon:");  
+  data.results.forEach(pokemon => {  
+    console.log("-", pokemon.name);  
+  });  
+}
+listPokemons() */
+
+// Muestra solo los nombres
+
+// Identifica qué propiedades tiene cada elemento dentro de results
+
+// Investiga qué información extra se obtiene usando la URL de cada Pokémon (sin implementarlo aún)
+
+// Imprime la informacion por console.log.
+
+// Muestra también la URL de cada Pokémon.
+
+async function listPokemons() {  
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon");  
+  const data = await response.json();  
+
+  console.log("Lista de Pokémon:");  
+  data.results.forEach(pokemon => {  
+    console.log("-", pokemon.name);
+    console.log(Object.keys(pokemon))
+    console.log(pokemon.url)
+  });  
+}
+listPokemons()
